c69c421fc105d186a8b9181e1a835d3505d34b45
Mohammad Hosein Shahpouri <MH.Shahpouri@gmail.com>
2023-04-10 23:04:20 +0330

    :white_check_mark: GetAllMoviesQueryTest created

---
+++ Test/WaferMovie.Application.Test/Movies/Queries/GetAllMoviesQueryTest.cs
@@ -0,0 +1,35 @@
+﻿using Moq;
+using StackExchange.Redis;
+using WaferMovie.Application.Movies.Queries.GetAllMovies;
+
+namespace WaferMovie.Application.Test.Movies.Queries;
+
+public class GetAllMoviesQueryTest
+{
+    private readonly IApplicationDbContext dbContext = InMemoryDatabase.Create();
+    private readonly IMediator mediator;
+
+    public GetAllMoviesQueryTest()
+    {
+        var applicationAssembly = Assembly.Load("WaferMovie.Application");
+        var mockCacheDb = new Mock<IDatabase>();
+        var services = new ServiceCollection();
+
+        services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(applicationAssembly));
+        services.AddValidatorsFromAssembly(applicationAssembly);
+        services.AddSingleton(dbContext);
+        services.AddTransient(typeof(IPipelineBehavior<,>), typeof(ValidationPipelineBehavior<,>));
+        services.AddSingleton(mockCacheDb.Object);
+        mediator = services.BuildServiceProvider().GetService<IMediator>()!;
+    }
+
+    [Fact]
+    public async Task ShouldReturnAllMovies()
+    {
+        var result = await mediator.Send(new GetAllMoviesQuery());
+        result.Succeeded.ShouldBeTrue();
+        result.Data.Count.ShouldBe(dbContext.Movies.Count());
+        result.Data.All(a => dbContext.Movies.Contains(a)).ShouldBeTrue();
+        dbContext.Movies.All(a => result.Data.Contains(a)).ShouldBeTrue();
+    }
+}
